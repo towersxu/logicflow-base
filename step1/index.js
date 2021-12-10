3054abf34d40337703ce9d5092682d1057576007
xutao(续心) <987427795@qq.com>
2021-12-10 16:47:11 +0800

    initial commit

--- step1/index.js
+++ step1/index.js
@@ -12,20 +12,50 @@ lf.render({
       id: "1",
       type: "rect",
       x: 100,
-      y: 100
+      y: 100,
+      text: "矩形"
     },
     {
       id: "2",
       type: "circle",
       x: 300,
-      y: 200
-    }
-  ],
-  edges: [
+      y: 100,
+      text: "圆形"
+    },
     {
-      sourceNodeId: "1",
-      targetNodeId: "2",
-      type: "polyline"
+      id: "3",
+      type: "ellipse",
+      x: 500,
+      y: 100,
+      text: "椭圆"
+    },
+    {
+      id: "4",
+      type: "polygon",
+      x: 100,
+      y: 250,
+      text: "多边形"
+    },
+    {
+      id: "5",
+      type: "diamond",
+      x: 300,
+      y: 250,
+      text: "菱形"
+    },
+    {
+      id: "6",
+      type: "text",
+      x: 500,
+      y: 250,
+      text: "纯文本节点"
+    },
+    {
+      id: "7",
+      type: "rect",
+      x: 100,
+      y: 400,
+      text: "html节点"
     }
   ]
 });
